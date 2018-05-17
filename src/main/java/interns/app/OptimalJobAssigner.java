664aef9909507fce6657efc73d09d1ceabffcf51
Christopher Chianelli <chris.chianelli@gmail.com>
2018-05-17 14:33:12 -0400

    Made it run with exec:java, get input from user instead of files

--- src/main/java/interns/app/OptimalJobAssigner.java
+++ src/main/java/interns/app/OptimalJobAssigner.java
@@ -1,13 +1,11 @@
 package interns.app;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import interns.model.Intern;
 import interns.model.InternPlacements;
@@ -19,96 +17,90 @@ public class OptimalJobAssigner {
 
     public static void main(String[] args) throws FileNotFoundException {
         SolverFactory<InternPlacements> solverFactory = SolverFactory.createFromXmlResource(
-                "interns/app/solverConfig.xml");
+                                                                                            "interns/app/solverConfig.xml");
         Solver<InternPlacements> solver = solverFactory.buildSolver();
 
-        List<Placement> placements = getPlacements(args[0]);
-        Map<String, Placement> placementMap = placements.stream().collect(Collectors.toMap(p -> p.getPosition(), p -> p));
-        List<Intern> interns = getInterns(Arrays.asList(args).subList(1, args.length), placementMap);
+        Scanner input = new Scanner(System.in);
+        List<Placement> placements = getPlacements(input);
+        List<Intern> interns = getInterns(input, placements);
+        input.close();
 
         InternPlacements internPlacements = new InternPlacements(interns, placements);
-        
+
         solver.addEventListener(event -> {
             if (event.isEveryProblemFactChangeProcessed()) {
                 System.out.printf("New best solution found\n");
                 printSolution(event.getNewBestSolution());
             }
         });
-        
+
         System.out.printf("Finished loading data. Beginning solve.\n");
         internPlacements = solver.solve(internPlacements);
         System.out.printf("Solving finished.\n");
-       
+
         printSolution(internPlacements);
     }
-    
+
     private static void printSolution(InternPlacements internPlacements) {
         System.out.printf("Best score: Hard: %d, Soft: %d\n", internPlacements.getScore().getHardScore(), internPlacements.getScore().getSoftScore());
         System.out.printf("Placements:\n");
 
         for (Intern intern : internPlacements.getInternList()) {
-            System.out.println(intern.getName());
             if (intern.getPlacement() != null) {
-            System.out.printf("\t%s: %s (%d).\n", intern.getName(), intern.getPlacement().getPosition(), intern.getPlacementRanking());
-            }
-            else {
+                System.out.printf("\t%s: %s (%d).\n", intern.getName(), intern.getPlacement().getPosition(), intern.getPlacementRanking());
+            } else {
                 System.out.printf("\t%s: is unassigned.\n", intern.getName());
             }
         }
     }
-    
-    private static List<Placement> getPlacements(String fileName) throws FileNotFoundException {
+
+    private static List<Placement> getPlacements(Scanner fileReader) throws FileNotFoundException {
         List<Placement> placementList = new ArrayList<>();
-        File file = new File(fileName);
-        Scanner fileReader = new Scanner(file);
-        while (fileReader.hasNextLine()) {
+
+        System.out.printf("Enter the placements, in the format [POSITIONS AVALIABLE] [NAME]. Enter a blank line to end the list.\n");
+        System.out.flush();
+        while (true) {
             String line = fileReader.nextLine();
-            String[] parts = line.split(" ");
-            switch (parts.length) {
-                case 1:
-                    placementList.add(new Placement(parts[0], 1));
-                    break;
-                    
-                case 2:
-                    placementList.add(new Placement(parts[1], Integer.parseInt(parts[0])));
-                    break;
-                    
-                default:
-                    fileReader.close();
-                    throw new IllegalArgumentException("Bad line: " + line);
+            if (line.isEmpty()) {
+                break;
             }
+            int positionsAvaliable = Integer.parseInt(line.substring(0, line.indexOf(' ')));
+            String positionName = line.substring(line.indexOf(' ') + 1);
+            placementList.add(new Placement(positionName, positionsAvaliable));
         }
-        fileReader.close();
+
         return placementList;
     }
 
-    private static List<Intern> getInterns(List<String> internFileNames, Map<String, Placement> placementMap) throws FileNotFoundException {
+    private static List<Intern> getInterns(Scanner fileReader, List<Placement> placementList) throws FileNotFoundException {
         List<Intern> internList = new ArrayList<>();
-        for (String fileName : internFileNames) {
-            File file = new File(fileName);
-            Scanner fileReader = new Scanner(file);
-            int lineNum = 0;
-            String internName = null;
-            List<Placement> internRankings = new ArrayList<>();
-            
-            while (fileReader.hasNextLine()) {
-                String line = fileReader.nextLine();
-                switch (lineNum) {
-                    case 0:
-                        internName = line;
-                        break;
-                        
-                    default:
-                        internRankings.add(placementMap.get(line));
-                        break;
-                }
-                lineNum++;
+        while (true) {
+            System.out.printf("Intern name (blank to exit):\n");
+            System.out.flush();
+            String internName = fileReader.nextLine();
+
+            if (internName.isEmpty()) {
+                break;
             }
-            internList.add(new Intern(internName, internRankings));
-            fileReader.close();
+            List<List<Placement>> internRankings = new ArrayList<>();
+            placementList.forEach((p) -> internRankings.add(new ArrayList<>()));
+
+            for (Placement placement : placementList) {
+                System.out.printf("%s Ranking for %s:\n", internName, placement.getPosition());
+                int rank = fileReader.nextInt();
+                internRankings.get(rank - 1).add(placement);
+            }
+            fileReader.nextLine();
+
+            Map<Placement, Integer> internScoreRankings = new HashMap<>();
+            for (int i = 0; i < internRankings.size(); i++) {
+                final int SCORE = -(i * i);
+                internRankings.get(i).forEach(p -> internScoreRankings.put(p, SCORE));
+            }
+
+            internList.add(new Intern(internName, internScoreRankings));
         }
         return internList;
     }
-
 
 }
